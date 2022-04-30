58dc011c76ba8cae3c8e690ecc2ac9395595fd0d
mwingian <morowingerthemain@gmail.com>
2022-04-30 15:11:22 +0200

    Last stand

---
+++ README.txt
@@ -0,0 +1,4 @@
+1st clip
+2nd clip
+3d clip
+last clip
---
+++ js/script.js
@@ -0,0 +1,10 @@
+// let defaultName = 'Безимянный';
+// let userName = prompt('Введите ваше имя',defaultName);
+// alert('Hello ' + userName);
+
+
+let mainInfo = 'Привет';
+let secondaryInfo = 'Вася';
+
+console.log(`${mainInfo}` + ' ' + `${secondaryInfo}` + '!');
+
